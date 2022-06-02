d834c6e942624c24f3162068d8755b597e98ea74
Armin FalDiS <armin.fallah96@gmail.com>
2022-06-02 10:30:12 +0430

    Contract Init

---
+++ contract/src/lib.rs
@@ -0,0 +1,30 @@
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::near_bindgen;
+
+#[near_bindgen]
+#[derive(Default, BorshDeserialize, BorshSerialize)]
+pub struct Contract {
+    // CONTRACT STATE
+}
+
+#[near_bindgen]
+impl Contract {
+    // CONTRACT METHODS
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use near_sdk::test_utils::{get_logs, VMContextBuilder};
+    use near_sdk::{testing_env, AccountId};
+
+    /// returns mock context
+    fn get_context(predecessor: AccountId) -> VMContextBuilder {
+        let mut builder = VMContextBuilder::new();
+        builder.predecessor_account_id(predecessor);
+        builder
+    }
+
+    // TESTS HERE
+}
